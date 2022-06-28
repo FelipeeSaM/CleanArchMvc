7c54396836be8661bd918c3ae303c4086f799bdf
eu mesmo <luiz_@s>
2022-06-28 01:08:13 -0300

    Implementando repositorio de category e product para o banco de dados

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration {
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category> {
+        public void Configure(EntityTypeBuilder<Category> builder) {
+            builder.HasKey(p => p.Id);
+            builder.Property(n => n.Name).HasMaxLength(100).IsRequired();
+
+            //insere manualmente 3 registros no banco
+            builder.HasData(new Category(1, "Casa"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
+                );
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration {
+    public class ProductConfiguration : IEntityTypeConfiguration<Product> {
+        public void Configure(EntityTypeBuilder<Product> builder) {
+            builder.HasKey(i => i.Id);
+            builder.Property(n => n.Name).HasMaxLength(100).IsRequired();
+            builder.Property(o => o.Description).HasMaxLength(200).IsRequired(); //IsRequired = não nullable
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+            builder.HasOne(q => q.Category).WithMany(r => r.Products).HasForeignKey(s => s.CategoryId); //relacionamento 1 categoria tem muitos produtos, e categoryId é foreingkey
+        }
+    }
+}
